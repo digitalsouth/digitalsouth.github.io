fd7d1571bf38dd6df679fe43298e5520cc9ccd18
Rolando R <50704607+RolRodr@users.noreply.github.com>
2022-08-28 19:28:27 -0400

    added events panel and search feature

---
+++ _site/assets/js/events.js
@@ -0,0 +1,48 @@
+// Update function
+let updateGallery = (filter, cards) => {
+
+    if (filter != "all") {
+                cards.forEach(s => {
+                    let card_value = s.getAttribute("value");
+
+                    if (card_value.includes(filter) == false) {
+                        s.classList.add("scale-out");
+                        setTimeout(() => {
+                            s.classList.add("hide")
+                        }, 200)
+
+                    } else {
+                        if (s.classList.contains("scale-out")) {
+                            s.classList.remove("hide");
+                            setTimeout(() => { s.classList.remove("scale-out") }, 200);
+                            setTimeout(() => { s.classList.add("scale-in") }, 210);
+                        }
+                    }
+                })
+            } else {
+                cards.forEach(s => {
+                    s.classList.remove("hide");
+                    setTimeout(() => { s.classList.remove("scale-out") }, 200);
+                    setTimeout(() => { s.classList.add("scale-in") }, 210);
+                });
+
+            } 
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    let elem = document.querySelector('.browser-default');
+    
+    if(elem != null){
+        console.log("event listeners coming on");
+        let options = ""
+        let instance = M.FormSelect.init(elem, options);
+        let cards = document.querySelectorAll(".gallery-card");
+        
+
+        elem.addEventListener("change", e => {
+            let filter = e.target.value;
+            updateGallery(filter, cards);
+        });
+    }
+
+});
